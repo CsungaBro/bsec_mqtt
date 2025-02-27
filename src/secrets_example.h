8864a57e09c428153e68142288dbde173c4c2ee6
CsungaBro <csunga16@gmail.com>
2025-02-27 21:46:33 +0100

    example secret added

---
+++ src/secrets_example.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID WIFI Name
+#define WIFI_PASSWORD my_password
+#define MQTT_SERVER 192.168.4.2
+#define MQTT_PORT 1883
+#define MQTT_USERNAME me
+#define MQTT_PASSWORD my_password_2
